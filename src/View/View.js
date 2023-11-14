0e144218c769d7e1f55255299208dadd0e49e22b
민규 <xiahtic73@gmail.com>
2023-11-14 19:30:37 +0900

    feat: View 클래스 생성
    inputView, outputView를 View 클래스의 필드로 추가 (기존 모듈의 경로 및 매개변수도 변경함)

---
+++ src/View/View.js
@@ -0,0 +1,10 @@
+import InputView from './InputView.js';
+import OutputView from './OutputView.js';
+
+class View {
+  #inputView = InputView;
+
+  #outputView = OutputView;
+}
+
+export default View;
